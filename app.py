8a7e7aec2bce8d52f335ef36a4a0b2bbc73ee294
ebk <ekeymon@wabtec.com>
2020-08-11 13:03:49 -0500

    Updated python file

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+################################################
+# Step 2 - Climate App
+
 import datetime as dt
 import numpy as np
 import pandas as pd
@@ -9,14 +12,13 @@ from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
 
-
-#################################################
 # Database Setup
-#################################################
+
 engine = create_engine("sqlite:///hawaii.sqlite")
 
 # reflect an existing database into a new model
 Base = automap_base()
+
 # reflect the tables
 Base.prepare(engine, reflect=True)
 
@@ -27,15 +29,10 @@ Station = Base.classes.station
 # Create our session (link) from Python to the DB
 session = Session(engine)
 
-#################################################
 # Flask Setup
-#################################################
 app = Flask(__name__)
 
-
-#################################################
 # Flask Routes
-#################################################
 
 @app.route("/")
 def welcome():
